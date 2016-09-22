c1a6b530482c7c65a6b966f505d6e18c2f50def5
Jason Little <jasonlttl@gmail.com>
2016-09-22 00:25:26 -0400

    implements two getters

--- find_people.js
+++ find_people.js
@@ -1,5 +1,58 @@
 'use strict';
+var _ = require('lodash');
+var rp = require('request-promise');
+var ENDPOINT = 'http://directory.osu.edu/fpjson.php';
 
-function FindPeople() { }
+function FindPeople() {
+}
 
+/**
+ * Requests info by first and last names.
+ * @param last_name
+ * @param first_name
+ * @returns {*}
+ */
+FindPeople.prototype.fromName = function (last_name, first_name) {
+  return this.getFromName(last_name, first_name).then(
+    function (response) {
+      console.log('success - received info for ' + last_name + ', ' + first_name);
+      return response.body;
+    }
+  );
+};
+
+FindPeople.prototype.getFromName = function (last_name, first_name) {
+  var options = {
+    method: 'GET',
+    uri: ENDPOINT + '?lastname=' + last_name + '&firstname=' + first_name,
+    resolveWithFullResponse: true,
+    json: true
+  };
+  return rp(options);
+};
+
+
+/**
+ * Requests info by name.n.
+ * @param username
+ * @returns {*}
+ */
+FindPeople.prototype.fromUsername = function (username) {
+  return this.getFromUsername(username).then(
+    function (response) {
+      console.log('success - received info for ' + username);
+      return response.body;
+    }
+  );
+};
+
+FindPeople.prototype.getFromUsername = function (username) {
+  var options = {
+    method: 'GET',
+    uri: ENDPOINT + '?name_n=' + username,
+    resolveWithFullResponse: true,
+    json: true
+  };
+  return rp(options);
+};
 module.exports = FindPeople;
--- test/test_find_people.js
+++ test/test_find_people.js
@@ -1,8 +1,39 @@
 'use strict';
 var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 var expect = chai.expect;
 var FindPeople = require('../find_people');
+chai.config.includeStack = true;
 
-describe('FindPeople', function() {
-    var subject = new FindPeople();
+describe('FindPeople', function () {
+  var fp = new FindPeople();
+
+  describe('#fromName', function () {
+    context('with a first and last name', function () {
+      it('returns a match', function () {
+
+        var last_name = 'Drake';
+        var first_name = 'Michael';
+        var value = fp.fromName(last_name, first_name).then(function (obj) {
+          return obj[0].last_name;
+        });
+        return expect(value).to.eventually.eq(last_name);
+      });
+    });
+  });
+
+  describe('#fromUsername', function () {
+    context('with a name.n', function () {
+      it('returns a match', function () {
+
+        var username = 'drake.379';
+        var value = fp.fromUsername(username).then(function (obj) {
+          return obj[0].username;
+        });
+        return expect(value).to.eventually.eq(username);
+      });
+    });
+  });
+
 });
