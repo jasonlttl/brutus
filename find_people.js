20d7f174e55bd8bba10e92d1eb05e634af3a58e3
Jason Little <jasonlttl@gmail.com>
2016-09-21 23:58:25 -0400

    adds fp lib and test stubs

---
+++ find_people.js
@@ -0,0 +1,5 @@
+'use strict';
+
+function FindPeople() { }
+
+module.exports = FindPeople;
---
+++ test/test_find_people.js
@@ -0,0 +1,8 @@
+'use strict';
+var chai = require('chai');
+var expect = chai.expect;
+var FindPeople = require('../find_people');
+
+describe('FindPeople', function() {
+    var subject = new FindPeople();
+});
